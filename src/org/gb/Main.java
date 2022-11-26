4a0304304afbef356a99b3db9e4318f065e7cb43
Maksim_I <maksim77775@yandex.ru>
2022-11-26 21:24:46 +0500

    init

---
+++ src/org/gb/Main.java
@@ -0,0 +1,9 @@
+package org.gb;
+
+public class Main {
+    public static void main(String[] args) {
+        // имя файла должно совпадать с именем класса (public)
+        // классы называются в стиле CamelCase
+        System.out.println("Привет");
+    }
+}
